2b1d8d60aabe846f45740274d5d79959d945cfdf
Tinh Ha <tinh.ha@officeatwork.com>
2025-07-31 10:11:03 +0700

    test ui change

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import { PageContent } from "../_page_content/page-content";
 const Home = () => {
     return (
         <PageContent>
-            <div>this his home page</div>
+            <div>this his home page changed</div>
         </PageContent>
     );
 };
